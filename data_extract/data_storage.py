27121ab0b0404ab5d2b070bcfe9474652ff7af04
Rui Pereira <ruiarpereira15@gmail.com>
2024-10-08 00:55:39 +0100

    :sparkles: Add read and output CSV options to DataStorage

--- data_extract/data_storage.py
+++ data_extract/data_storage.py
@@ -23,6 +23,17 @@ class DataStorage:
         
         return df
 
+    def read_csv(self, path: str, schema: Optional[Dict[str, pl.DataType]] = None) -> pl.DataFrame:
+        logger.info(f'Reading CSV from: {path}')
+        df = pl.read_csv(path)
+        if schema:
+            # Convert DataFrame columns to the specified data types
+            for column, dtype in schema.items():
+                logger.info(f'Column : {column}, dtype : {dtype}')
+                df = df.with_columns(pl.col(column).cast(dtype))
+        
+        return df
+
     def output_excel(self, path: str, df: pl.DataFrame, schema: Optional[Dict[str, pl.DataType]] = None, append: bool = False) -> None:
         logger.info(f'Outputting Excel to: {path}')
         if schema:
@@ -36,3 +47,19 @@ class DataStorage:
             df = pl.concat([existing_df, df])
         
         df.write_excel(path)
+        logger.info('Successfully updated XLSX')
+
+    def output_csv(self, path: str, df: pl.DataFrame, schema: Optional[Dict[str, pl.DataType]] = None, append: bool = False) -> None:
+        logger.info(f'Outputting CSV to: {path}')
+        if schema:
+            # Convert DataFrame columns to the specified data types
+            for column, dtype in schema.items():
+                logger.info(f'Column :{column}, dtype :{dtype}')
+                df = df.with_columns(pl.col(column).cast(dtype))
+        
+        if os.path.exists(path) and append:
+            existing_df = self.read_csv(path=path, schema=schema)
+            df = pl.concat([existing_df, df])
+        
+        df.write_csv(path)
+        logger.info('Successfully updated CSV')
