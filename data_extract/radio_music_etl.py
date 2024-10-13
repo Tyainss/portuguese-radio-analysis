d3d93313158520e2903763ce25ba6d2b02dee513
Rui Pereira <ruiarpereira15@gmail.com>
2024-10-13 20:23:40 +0100

    :sparkles: Add identification of unregistered artists

--- data_extract/radio_music_etl.py
+++ data_extract/radio_music_etl.py
@@ -2,7 +2,7 @@ import polars as pl
 
 from config_manager import ConfigManager
 from data_storage import DataStorage
-from radio_scraper import RFMRadioScraper, MegaHitsRadioScraper, PassouTypeRadioScraper
+from radio_scraper import  PassouTypeRadioScraper, RFMRadioScraper, MegaHitsRadioScraper
 from genius_api import GeniusAPI
 from musicbrainz_api import MusicBrainzAPI
 from spotify_api import SpotifyAPI
@@ -23,14 +23,22 @@ class RadioMusicETL:
         self.mb_api = MusicBrainzAPI()
         self.spotify_api = SpotifyAPI()
         self.wikipedia_api = WikipediaAPI()
+        self._initialize_column_names()
+
+    def _initialize_column_names(self):
+        self.RADIO_COLUMN = self.config_manager.RADIO_COLUMN
+        self.DAY_COLUMN = self.config_manager.DAY_COLUMN
+        self.TIME_PLAYED_COLUMN = self.config_manager.TIME_PLAYED_COLUMN
+        self.TRACK_TITLE_COLUMN = self.config_manager.TRACK_TITLE_COLUMN
+        self.TRACK_ARTIST_COLUMN = self.config_manager.TRACK_ARTIST_COLUMN
 
     def _identify_unregistered_tracks(self, new_track_df, registered_tracks):
         # Combine TRACK_TITLE and TRACK_ARTIST to create a unique identifier
         new_track_df = new_track_df.with_columns(
-            pl.concat_str(["TRACK_TITLE", "TRACK_ARTIST"], separator=" - ").alias("track_id")
+            pl.concat_str([self.TRACK_TITLE_COLUMN, self.TRACK_ARTIST_COLUMN], separator=" - ").alias("track_id")
         )
         registered_tracks = registered_tracks.with_columns(
-            pl.concat_str(["TRACK_TITLE", "TRACK_ARTIST"], separator=" - ").alias("track_id")
+            pl.concat_str([self.TRACK_TITLE_COLUMN, self.TRACK_ARTIST_COLUMN], separator=" - ").alias("track_id")
         )
 
         # Find tracks in new_track_df that are not in registered_tracks
@@ -39,8 +47,17 @@ class RadioMusicETL:
         )
 
         # Select only TRACK_TITLE and TRACK_ARTIST columns and remove duplicates
-        result = unregistered_tracks.select(["TRACK_TITLE", "TRACK_ARTIST"]).unique()
+        result = unregistered_tracks.select([self.TRACK_TITLE_COLUMN, self.TRACK_ARTIST_COLUMN]).unique()
 
+        return result
+    
+    def _identify_unregistered_artists(self, new_artist_df, registered_artists):
+
+        unregistered_artists = new_artist_df.filter(
+            ~pl.col(self.TRACK_ARTIST_COLUMN).is_in(registered_artists[self.TRACK_ARTIST_COLUMN])
+        )
+        result = unregistered_artists.select([self.TRACK_ARTIST_COLUMN]).unique()
+        
         return result
 
 
@@ -65,10 +82,16 @@ class RadioMusicETL:
             path=self.config_manager.TRACK_INFO_CSV_PATH,
             schema=self.config_manager.TRACK_INFO_SCHEMA
         )
+        already_extracted_artists = already_extracted_data.select([self.TRACK_ARTIST_COLUMN])
 
         new_tracks = self._identify_unregistered_tracks(
             new_track_df=combined_df,
             registered_tracks=already_extracted_data
+        )
+
+        new_artists = self._identify_unregistered_artists(
+            new_artist_df=combined_df,
+            registered_artists=already_extracted_artists
         )
 
         # Check Track Info data extracted
