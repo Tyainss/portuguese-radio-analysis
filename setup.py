c66588f4aa55dd7b86d35ad41f2cbebfc327ee06
Rui Pereira <ruiarpereira15@gmail.com>
2025-01-19 19:18:41 +0000

    :bug: Fix dependency issues

--- setup.py
+++ setup.py
@@ -3,13 +3,11 @@ from setuptools import setup, find_packages
 # Read requirements.txt for dependencies
 def parse_requirements():
     with open('requirements.txt') as f:
-        lines = f.read().splitlines()
-        # Filter out -e and git+ lines
-        return [line for line in lines if not line.startswith('-e') and 'git+' not in line]
+        return f.read().splitlines()
 
 setup(
     name='radio_song_analysis',
-    version='0.1',
+    version='0.2',
     author='Rui Pereira',
     author_email='ruiarpereira15@gmail.com',
     description='A Streamlit app for radio song sentiment analysis',
@@ -20,9 +18,6 @@ setup(
     # packages=find_packages(), 
     include_package_data=False,  # Ensures non-Python files are included
     install_requires=parse_requirements(),  # Install dependencies from requirements.txt
-    dependency_links=[
-        "git+https://github.com/Tyainss/radio_song_analysis@da24e1546d82fb55dd15bbac0a19fb87236a8dbb#egg=radio_song_analysis"
-    ],
     python_requires='>=3.8',  # Specify Python version compatibility
     classifiers=[
         'Programming Language :: Python :: 3',
